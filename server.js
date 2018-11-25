381a5ec1edd363ef05fb13e62d8b4d8589743e0a
Daeun Yim <deyim33@gmail.com>
2018-11-25 16:00:10 -0500

    path finding added

--- server.js
+++ server.js
@@ -1,34 +1,79 @@
+const exphbs = require('express-handlebars');
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const http = require('http');
+const Graph = require('node-dijkstra')
 const socketIO = require('socket.io');
 var ss = require('socket.io-stream');
 const port = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-
 const publicPath = path.join(__dirname,'/public');
 app.use(express.static(publicPath));
 
-var findPath = ()=>{
-	;
-	return [1,2,3];
-}
 
-server.listen(port, ()=>{
-    console.log("Starting the server...");
+// views
+var hbs = exphbs.create({
+    defaultLayout: 'main',
+    partialsDir: path.join(__dirname, '/views/partials'),
+    layoutsDir: path.join(__dirname, './views/layouts')
+});
+
+app.use(express.static('public'));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '/views'));
+
+var route = new Graph()
+var myPath = [], endPoint = 'A', startPoint = 'A';
+
+
+function map_to_object(map) {
+    const out = Object.create(null)
+    map.forEach((value, key) => {
+      if (value instanceof Map) {
+        out[key] = map_to_object(value)
+      }
+      else {
+        out[key] = value
+      }
+    })
+    return out
+ }
+
+app.get('/ready', (req,res)=>{
+	res.render('pathRequest')
 })
 
-var myPath = findPath();
+app.get('/path_request', (req,res)=>{	
+	route.addNode('A', { 'B':1 })
+	route.addNode('B', { 'A':1, 'C':2, 'D': 4 })
+	route.addNode('C', { 'B':2, 'D':1 })
+	route.addNode('D', { 'C':1, 'B':4 })
 
-io.on('connection', (socket)=>{
+	console.log(route.graph)
+	var myRoute = new Object()
+	myRoute = map_to_object(route.graph)
+	console.log(myRoute)
+	res.send(myRoute)
+});
+
+app.post('/path_request', (req,res)=>{
+	startPoint = req.body.startPoint
+	endPoint = req.body.endPoint
+	myPath = route.path(startPoint, endPoint)
+	myObjData = {route, path:myPath}
+	res.redirect('/navigation');
+})
+
+app.get('/navigation', (req,res)=>{
+	var navigation = io.of('/navigation');
+	navigation.on('connection', (socket)=>{
     console.log('raspberrypi connected');
 
-    socket.emit('turn on', {pi: myPath[0]});
-    
+    socket.emit('turn on', {pi: myPath[0]});    
 
     i = 1
 	socket.on('on dock', function(msg, fn){
@@ -43,10 +88,16 @@ io.on('connection', (socket)=>{
 	    }		
 	});
 
-
 	socket.on('disconnect', ()=>{
 	    console.log('browser shutdown');
-	});	  
-
-    
+	});    
 })
+
+})
+
+
+
+server.listen(port, ()=>{
+    console.log("Starting the server...");
+})
+
