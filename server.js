eeebadfda51dd1851eb79420f2006ccfc871cdf9
Daeun Yim <deyim33@gmail.com>
2018-11-30 15:39:26 -0500

    socket io part completed

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const fs = require('fs');
 const express = require('express');
 const http = require('http');
 const Graph = require('node-dijkstra')
-const socketIO = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 var bodyParser = require('body-parser')
 var ss = require('socket.io-stream');
 const port = process.env.PORT || 3000;
-var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
+
+
+// var io = socketIO(server);
 const publicPath = path.join(__dirname,'/public');
 app.use(express.static(publicPath));
 
@@ -32,8 +34,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '/views'));
 
-// var route = new Graph()
-// var myPath = [], endPoint = 'A', startPoint = 'A';
+
 global.route = new Graph()
 global.myPath = [], global.endPoint = 'A', global.startPoint = 'A';
 
@@ -48,50 +49,56 @@ function map_to_object(map) {
       }
     })
     return out
- }
+}
 
-io.on('connection', (socket)=>{
-	console.log('raspberrypi connected');
-	socket.emit('connection')
-
-	socket.emit('turn on', {pi: myPath[0]});    
-
+var nav = io.of('navigation')
+nav.on('connection', function(socket){
+    console.log('raspberrypi connected');
+	// nav.emit('connection')
+	// socket.emit('turn on', {pi: myPath[0]}); 
 	i = 1
-	socket.on('on dock', function(msg, fn){
+	// socket.emit('turn on', {pi: myPath[0]});
+	nav.on('start', function(msg){
+		nav.broadcast.emit('turn on', {pi: myPath[0]});		
+	})
+	nav.on('on dock', function(msg, fn){
 		console.log(msg.pi, " is on the dock");
 		if (i == myPath.length){
+			nav.broadcast.emit('finish',{pi: 'Destination'})
 			console.log("Destination Arrived!!");
+
 		}
 		else{
 			console.log("turn on ", myPath[i]);
-			socket.broadcast.emit('turn on', {pi: myPath[i]});
+			nav.broadcast.emit('turn on', {pi: myPath[i]});
 			i++;
 		}		
 	});
 
-	socket.on('disconnect', ()=>{
+	nav.on('disconnect', ()=>{
 		console.log('browser shutdown');
 	});    
+});
+
+app.get('/navigation', (req,res)=>{
+	// res.sendFile(__dirname + '/public/index.html');
+	res.render('navigation', {path: myPath[0]})
 })
 
-app.get('/index', (req,res)=>{
-	console.log("HI");
-	res.render('index');
-})
 app.get('/ready', (req,res)=>{	
 	res.render('pathRequest')
 })
 
 app.get('/path_request', (req,res)=>{	
-	route.addNode('A', { 'D':1, 'E':1 })
-	route.addNode('B', { 'F':1 })
-	route.addNode('C', { 'E':1, 'F':1 })
-	route.addNode('D', { 'G':1 })
-	route.addNode('E', { 'H':1, 'I':1 })
-	route.addNode('F', { 'H':1 })
-	route.addNode('G', {})
-	route.addNode('H', { 'I':1 })
-	route.addNode('I', {})
+	route.addNode('A', { 'B':1, 'D':1, 'E':1 })
+	route.addNode('B', { 'A':1, 'F':1 })
+	route.addNode('C', { 'F':1, 'E':1 })
+	route.addNode('D', { 'A':1, 'G':1 })
+	route.addNode('E', { 'A':1, 'C':1, 'I':1 })
+	route.addNode('F', { 'C':1, 'B':1, 'H':1 })
+	route.addNode('G', { 'D':1 })
+	route.addNode('H', { 'I':1, 'F':1})
+	route.addNode('I', { 'E':1, 'H':1})
 
 	console.log(route.graph);
 	var myRoute = new Object();
@@ -103,15 +110,10 @@ app.post('/path_request', (req,res)=>{
 	startPoint = req.body.startPoint
 	endPoint = req.body.endPoint
 	myPath = route.path(startPoint, endPoint)
+	console.log(startPoint, endPoint)
 	console.log("node post path_request",myPath)
 	res.redirect('/navigation');
 })
-
-app.get('/navigation', (req,res)=>{
-	res.render('navigation', {myPath:myPath})	
-})
-
-
 
 server.listen(port, ()=>{
     console.log("Starting the server...");
